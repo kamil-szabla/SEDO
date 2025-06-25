8af5aff48c2af760e623f9e0b59e860f0aeaef8f
Kamil Szabla <ksszabla@amazon.com>
2025-06-25 18:49:10 +0100

    Initialize dummmy records and users

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,8 @@
-from app import create_app, db
+from app import create_app
+from db_init import init_db
 
 app = create_app()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-        print("Database tables created.")
+    init_db()
     app.run(debug=True, port=5001)
