464fd43e9f47d2c054f91e5dfb7cefbfe648ea16
Kamil Szabla <ksszabla@amazon.com>
2025-07-25 18:12:06 +0100

    Update session cookie flag

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -19,7 +19,7 @@ def create_app(config_overrides=None):
 
     app.config.update({
         "SESSION_COOKIE_HTTPONLY": True,
-        "SESSION_COOKIE_SAMESITE": "None",
+        "SESSION_COOKIE_SAMESITE": "Lax",
         "SESSION_COOKIE_SECURE": True, 
         "SESSION_COOKIE_PATH": "/",
         # "SESSION_COOKIE_DOMAIN": ".onrender.com",
